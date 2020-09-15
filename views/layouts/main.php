982876f615dd1f1afc9c8694a32388f24d76aa29
hasan kiwan <hassankiwan92@gmail.com>
2020-09-16 01:48:55 +0300

    mm

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -36,30 +36,39 @@ AppAsset::register($this);
         ],
     ]);
     echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],        
+    ]);
+
+    
+    if (Yii::$app->user->isGuest) {
+        $menuItems[] = ['label' => 'Home', 'url' => ['/site/index']];
+        $menuItems[] = ['label' => 'About', 'url' => ['/site/about']];
+        $menuItems[] =['label' => 'Contact', 'url' => ['/site/contact']];
+        $menuItems[] =['label' => 'Login', 'url' => ['/site/login']];
+        
+      
+    }else{
+         
+        $menuItems[] = ['label' => 'Home', 'url' => ['/site/index']];
+        $menuItems[] =['label' => 'Users', 'url' => ['/users/index']];
+        $menuItems[] = ['label' => 'Area', 'url' => ['/area/index']];
+        $menuItems[] = ['label' => 'Sender', 'url' => ['/sender/index']];
+        $menuItems[] = ['label' => 'Status', 'url' => ['/status/index']];
+        $menuItems[] = ['label' => 'Payments', 'url' => ['/payments/index']];
+        $menuItems[] =['label' => 'Subscription', 'url' => ['/subscription/index']];
+        $menuItems[] = '<li>'
+        . Html::beginForm(['/site/logout'], 'post')
+        . Html::submitButton(
+            '( ' . Yii::t('app', 'Logout') . ' ' . Yii::$app->user->identity->username . ')',
+            ['class' => 'btn btn-link logout']
+        )
+        . Html::endForm()
+        . '</li>';
+
+    }
+    echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            ['label' => 'Users', 'url' => ['/users/index']],
-            ['label' => 'Area', 'url' => ['/area/index']],
-            ['label' => 'Sender', 'url' => ['/sender/index']],
-            ['label' => 'Status', 'url' => ['/status/index']],
-            ['label' => 'Payments', 'url' => ['/payments/index']],
-            ['label' => 'Subscription', 'url' => ['/subscription/index']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
+        'items' => $menuItems,
     ]);
     NavBar::end();
     ?>
